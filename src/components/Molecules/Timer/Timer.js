15d43fff41665055e7558bbcfe23defdfc37b563
szymonqqaz <szymonqqaz22@gmail.com>
2019-08-27 09:50:13 +0200

    Timer is warking,and add buttons

--- src/components/Molecules/Timer/Timer.js
+++ src/components/Molecules/Timer/Timer.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+/* eslint-disable no-nested-ternary */
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
 
 const StyledTimerWarpper = styled.div`
   width: 100%;
@@ -22,60 +23,87 @@ const WrapperButtons = styled.div`
   bottom: 0;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
+`;
+
+const StyledButton = styled.button`
+  height: 48px;
+  width: 30%;
+  color: white;
+  font-weight: 500;
+  background: linear-gradient(90deg, #6762ff 0%, rgba(255, 255, 255, 0) 100%), #ff5362;
+  border-radius: 10px;
+  border: none;
+  font-size: 2.5rem;
+
+  ${({ red }) =>
+    red &&
+    css`
+      background: ${({ theme }) => theme.red};
+    `}
+
+  ${({ blue }) =>
+    blue &&
+    css`
+      background: ${({ theme }) => theme.blue};
+    `}
 `;
 
 const Timer = () => {
-  const [secounds, setSecounds] = useState('00');
-  const [minutes, setMinutes] = useState(25);
-  const [myInterval, setMyInterval] = useState();
-  const [currentSecounds, setCurrentSecounds] = useState(1500);
-
   const addZero = item => {
     return item < 10 ? `0${item}` : item;
   };
 
-  const defaultTime = 1500;
+  const [test, setTest] = useState(1500);
+  const [timer, setTimer] = useState('reset');
 
-  const run = () => {
-    setMyInterval(
-      setInterval(() => {
-        if (currentSecounds === 0) {
-          clearInterval(myInterval);
-        } else {
-          setCurrentSecounds(currentSecounds - 1);
-        }
-        let min = Math.floor(currentSecounds / 60);
-        min %= 60;
-        const sec = currentSecounds % 60;
+  useEffect(() => {
+    let myTimer;
+    if (timer === 'run') {
+      myTimer = setInterval(() => {
+        setTest(prev => prev - 1);
+      }, 100);
+    } else if (timer === 'reset') {
+      setTest(1500);
+    }
+    return () => {
+      clearInterval(myTimer);
+    };
+  }, [timer]);
 
-        setSecounds(addZero(sec));
-        setMinutes(addZero(min));
-      }, 1000),
-    );
-  };
-
-  const pauseTimer = () => {
-    clearInterval(myInterval);
-    console.log(currentSecounds);
-  };
-
-  const reset = () => {
-    setSecounds('00');
-    setMinutes(25);
-    setCurrentSecounds(defaultTime);
-    clearInterval(myInterval);
-  };
+  let sec = test % 60;
+  sec = addZero(sec);
+  const minutes = Math.floor(test / 60);
+  let min = minutes % 60;
+  min = addZero(min);
 
   return (
     <StyledTimerWarpper>
       <StyledTimer>
-        {minutes} : {secounds}
+        {min} : {sec}
       </StyledTimer>
       <WrapperButtons>
-        <button onClick={() => run()}>frf</button>
-        <button onClick={() => pauseTimer()}>pause</button>
-        <button onClick={() => reset()}>reset</button>
+        {/* @TODO: przerobić, zrobić całe review */}
+        {timer === 'reset' ? (
+          <>
+            <StyledButton onClick={() => setTimer('run')}>start</StyledButton>
+          </>
+        ) : timer === 'pause' ? (
+          <>
+            <StyledButton red onClick={() => setTimer('reset')}>
+              reset
+            </StyledButton>
+            <StyledButton blue onClick={() => setTimer('run')}>
+              wznów
+            </StyledButton>
+          </>
+        ) : (
+          <>
+            <StyledButton blue onClick={() => setTimer('pause')}>
+              pause
+            </StyledButton>
+          </>
+        )}
       </WrapperButtons>
     </StyledTimerWarpper>
   );

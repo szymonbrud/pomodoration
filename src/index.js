2bc3d701d092d7534c26dbcf120b49d54e4edb86
szymonqqaz <szymonqqaz22@gmail.com>
2019-09-16 19:15:55 +0200

    I add notification witch I can call from postman

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'storeConfig';
 // import * as serviceWorker from './firebase-messaging-sw.js';
+import firebase from 'firebase';
+import { askForPermissioToReceiveNotifications } from 'firebaseConfig';
 import App from './App';
 
 ReactDOM.render(
@@ -11,7 +13,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
+askForPermissioToReceiveNotifications();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

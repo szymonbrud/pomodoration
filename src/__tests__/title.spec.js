85e8dc12464c67cb086d5c547f0c666fa4c2f64c
szymonqqaz <szymonqqaz22@gmail.com>
2019-10-12 15:25:25 +0200

    write a waking good integration tests

--- src/__tests__/title.spec.js
+++ src/__tests__/title.spec.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import firebase from 'firebase';
-import { render, fireEvent, cleanup } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { applyMiddleware, compose, createStore, combineReducers, dispatch } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import historyOfPomodoro from 'reducers/historyOfPomodoro';
 import { historyOfPomodorosDataFromDatabase } from 'Utils/dataToTests';
-import { downloadSessions, getDateFromDataAndSort } from 'actions/downloadSessionsFromDatabase';
-
-// jest.mock('firebase');
-// jest.mock('actions/downloadSessionsFromDatabase');
-
-// const allStoreEnchancers = compose(applyMiddleware(thunk));
+import {
+  getDateFromDataAndSort,
+  downloadPomodoroSessions,
+  downloadData,
+} from 'actions/downloadSessionsFromDatabase';
 
 const allReducers = combineReducers({
   historyOfPomodoro,
 });
-
-// const store = createStore(allReducers, allStoreEnchancers);
 
 const testStore = initalState => {
   const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
@@ -25,39 +18,39 @@ const testStore = initalState => {
 };
 
 // @TODO: change names
+// @TODO: jak działają fn() oraz jak powino się nazywać it
 describe('test reducers and action copatibility', () => {
-  it('shouldf ', () => {
+  it('Should work ', () => {
     const store = testStore();
-
+    store.dispatch(downloadPomodoroSessions(historyOfPomodorosDataFromDatabase));
     store.dispatch(getDateFromDataAndSort(historyOfPomodorosDataFromDatabase));
-    console.log(store.getState());
+    store.dispatch(downloadData(true));
+
+    const shouldToReturn = {
+      historyOfPomodoro: {
+        allDate: ['28|09|2019', '27|09|2019', '26|09|2019'],
+        downloadData: true,
+        pomodoroSessions: [
+          {
+            date: 1569575447927,
+            dateSerch: '27|09|2019',
+            pomodoro: 6,
+            time: 8400,
+            title: 'programowanie',
+          },
+          { date: 1569602445200, dateSerch: '26|09|2019', pomodoro: 1, time: 3, title: 'ramnulus' },
+          { date: 1569672015817, dateSerch: '28|09|2019', pomodoro: 1, time: 1, title: 'rabarbar' },
+          {
+            date: 1569575447927,
+            dateSerch: '27|09|2019',
+            pomodoro: 6,
+            time: 8400,
+            title: 'programowanie',
+          },
+        ],
+      },
+    };
+
+    expect(store.getState()).toEqual(shouldToReturn);
   });
 });
-
-// const InitStateComp = cmp => {
-//   return render(<Provider store={store}>{cmp}</Provider>);
-// };
-
-// firebase.auth.mockImplementationOnce(() => {
-//   return {
-//     currentUser: {
-//       uid: null,
-//     },
-//   };
-// });
-
-// firebase.database.mockImplementationOnce(() => {
-//   return {
-//     ref: () => {
-//       return {
-//         orderByChild: () => {
-//           return {
-//             once: () => Promise.resolve({ historyOfPomodorosDataFromDatabase }),
-//           };
-//         },
-//       };
-//     },
-//   };
-// });
-
-// downloadSessions();
